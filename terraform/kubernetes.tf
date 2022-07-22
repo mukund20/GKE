1852004c34e1d6f9fa073d1b23282863885cfeb0
mukund20 <mukundatolay20@gmail.com>
2022-07-22 12:24:56 +0530

    removed logging and monitoring

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -6,8 +6,6 @@ resource "google_container_cluster" "primary" {
   initial_node_count       = 1
   network                  = google_compute_network.main.self_link
   subnetwork               = google_compute_subnetwork.private.self_link
-  logging_service          = "logging.googleapis.com/kubernetes"
-  monitoring_service       = "monitoring.googleapis.com/kubernetes"
   networking_mode          = "VPC_NATIVE"
 
   # Optional, if you want multi-zonal cluster
